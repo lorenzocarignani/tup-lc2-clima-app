d44dddeb2f144859f8b5527c24f220caab2fb304
Lorenzo Carignani <lorenzocarignani@outlook.com>
2022-11-07 09:55:57 -0300

    Terminado

---
+++ js/pages/add-city.js
@@ -0,0 +1,52 @@
+async function validateCity(newCity) {
+    let cities = getCitiesFromLocalStorage();
+  
+    for (let i = 0; i < cities.length; i++) {
+        if (newCity == cities[i]) {
+            return "warning";
+        };
+    };
+
+    if (await consultAPI(newCity) == "error") {
+        return "error";
+    }
+    else {
+        return "success";
+    };
+}
+
+function removeMessage() {
+    setTimeout(function() {
+        document.getElementsByClassName("alert")[0].remove();
+    }, 3000);
+}
+
+async function addCityToLocalStorage() {
+    let cities = getCitiesFromLocalStorage();
+    let newCity = document.getElementById("inputCity").value;
+    newCity = newCity.toUpperCase()
+
+    switch(await validateCity(newCity)) {
+        case "success":
+            cities.push(newCity);
+            localStorage.setItem("CITIES", JSON.stringify(cities));
+            document.getElementById("messajeBox").innerHTML += successMessage;
+            removeMessage();
+            break;
+        case "warning":
+            document.getElementById("messajeBox").innerHTML += warningMessage;
+            removeMessage();
+            break;
+        case "error":
+            document.getElementById("messajeBox").innerHTML += errorMessage;
+            removeMessage();
+            break;
+    };
+};
+
+let successMessage = '<p class="alert success">Ciudad agregada con éxito</p>';
+let errorMessage = '<p class="alert error">Error: La ciudad ingresada no se encuenta en la API o se produjo un error al consultar</p>';
+let warningMessage = '<p class="alert warning">La ciudad ingresada ya se encuentra almacenada</p>';
+
+let buttonAddCity = document.getElementById("buttonAdd");
+buttonAddCity.addEventListener("click", addCityToLocalStorage);
